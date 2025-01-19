1885ea6fcc9f394ef0eb915d5ec685b9a74113cb
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-01-19 15:40:02 -0300

    [ :card_file_box: ] | setup: db - migration > history (app::data)

--- js.learn.Data/database_script/LearnDB.sql
+++ js.learn.Data/database_script/LearnDB.sql
@@ -11,6 +11,15 @@ GO
 USE LearnDB
 GO
 
+IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+	CREATE TABLE [__EFMigrationsHistory] (
+		[MigrationId] nvarchar(150) NOT NULL,
+		[ProductVersion] nvarchar(32) NOT NULL,
+		CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+	);
+END;
+GO
 
 
 
