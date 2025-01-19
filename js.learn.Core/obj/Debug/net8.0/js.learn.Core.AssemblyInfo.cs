1433783d3e68bde443a947b571aabbfdfe7e5f2c
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-01-19 12:52:14 -0300

    [ :heart: ] | create: lib - core (app::layers)

--- js.learn.Core/obj/Debug/net8.0/js.learn.Core.AssemblyInfo.cs
+++ js.learn.Core/obj/Debug/net8.0/js.learn.Core.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("js.learn.api")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("js.learn.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e0cb639794a20f6d8e7b7032c85806dfadd63ce")]
-[assembly: System.Reflection.AssemblyProductAttribute("js.learn.api")]
-[assembly: System.Reflection.AssemblyTitleAttribute("js.learn.api")]
+[assembly: System.Reflection.AssemblyProductAttribute("js.learn.Core")]
+[assembly: System.Reflection.AssemblyTitleAttribute("js.learn.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
