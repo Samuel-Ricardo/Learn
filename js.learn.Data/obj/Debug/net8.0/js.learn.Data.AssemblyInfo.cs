694d6144272cd7261a9e19af8b5394439840b8da
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-01-19 18:39:34 -0300

    [ :heavy_plus_sign: ] | add: dep - entity framework & azure containers (app::data)

--- js.learn.Data/obj/Debug/net8.0/js.learn.Data.AssemblyInfo.cs
+++ js.learn.Data/obj/Debug/net8.0/js.learn.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("js.learn.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4618350ceffcfce5d88c9d779d29d3b0a951d379")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9736876b51656a70417724c5a24763e20cc048e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("js.learn.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("js.learn.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- js.learn.api/obj/Debug/net8.0/js.learn.api.AssemblyInfo.cs
+++ js.learn.api/obj/Debug/net8.0/js.learn.api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("js.learn.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1389c932bc9001fc08e678200df258a5823ac257")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9736876b51656a70417724c5a24763e20cc048e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("js.learn.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("js.learn.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
