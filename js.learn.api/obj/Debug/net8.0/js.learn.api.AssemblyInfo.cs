79669b8cd107aafee91b8ccd492923ff7df7b0a2
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-04-01 17:19:17 -0300

    [ :recycle: ] | run: build (module::api)

--- js.learn.api/obj/Debug/net8.0/js.learn.api.AssemblyInfo.cs
+++ js.learn.api/obj/Debug/net8.0/js.learn.api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("js.learn.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55f97b9b73dfe30381414ba521e75ba09dae3705")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41c19e5355def4cf250758e989c3056bcddbf17e")]
 [assembly: System.Reflection.AssemblyProductAttribute("js.learn.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("js.learn.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
