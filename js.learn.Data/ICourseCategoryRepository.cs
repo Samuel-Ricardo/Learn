ae016698051a5855a20bde2c2fc7922cc7a0223a
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-04-01 12:44:09 -0300

    [ :card_file_box: ] | create: repository - course > category (module::domain)

---
+++ js.learn.Data/ICourseCategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using js.learn.Core.Entities;
+
+namespace js.learn.Data
+{
+    public interface ICourseCategoryRepository
+    {
+        CourseCategory? GetById(int id);
+        List<CourseCategory> GetCourseCategories();
+    }
+}
