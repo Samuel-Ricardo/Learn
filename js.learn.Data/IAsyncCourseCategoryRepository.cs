c831925d1bb0dcc6768ed257bc1bad3140e5a726
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-04-01 12:55:45 -0300

    [ :card_file_box: ] | create: repository - course > category [async] (module::domain)

---
+++ js.learn.Data/IAsyncCourseCategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using js.learn.Core.Entities;
+
+namespace js.learn.Data
+{
+    internal interface IAsyncCourseCategoryRepository
+    {
+        Task<CourseCategory?> GetByIdAsync(int   id);
+        Task<List<CourseCategory>> GetCourseCategoriesAsync();
+    }
+}
