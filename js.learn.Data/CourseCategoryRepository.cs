3f43ef4f90cf7b7636b595fbb281aaeaca8ab0d6
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-04-01 12:44:45 -0300

    [ :card_file_box: ] | impl: repository - course > category (module::application)

---
+++ js.learn.Data/CourseCategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using js.learn.Core.Entities;
+using js.learn.Data.Entities;
+
+namespace js.learn.Data
+{
+    public class CourseCategoryRepository(LearnDbContext DbContext) : ICourseCategoryRepository
+    {
+
+        private readonly LearnDbContext _DbContext = DbContext;
+
+        public CourseCategory? GetById(int id)
+        {
+            return _DbContext.CourseCategories.Find(id);
+        }
+
+        public List<CourseCategory> GetCourseCategories()
+        {
+            return _DbContext.CourseCategories.ToList();
+        }
+    }
+}
