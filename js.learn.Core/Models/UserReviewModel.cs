514004b267ed951cf364206225a442662b00b2be
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-04-01 17:17:08 -0300

    [ :sparkles: ] | create: model - user > review (module::core)

---
+++ js.learn.Core/Models/UserReviewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace js.learn.Core.Models
+{
+    public class UserReviewModel
+    {
+        public int ReviewId { get; set; }
+        public int CourseId { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+
+        public int Rating { get; set; }
+
+        public string? Comments { get; set; }
+
+        public DateTime ReviewDate { get; set; }
+    }
+}
