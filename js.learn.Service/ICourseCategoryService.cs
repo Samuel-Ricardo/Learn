0d8efdff2282034115d50f774f75afaa8e3c148e
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-04-01 14:34:32 -0300

    [ :sparkle: ] | create: service - course > category (module::core)

---
+++ js.learn.Service/ICourseCategoryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using js.learn.Core.Models;
+
+namespace js.learn.Service
+{
+    public interface ICourseCategoryService
+    {
+        Task<CourseCategoryModel?> GetById(int id);
+        Task<List<CourseCategoryModel>> GetAll();
+    }
+}
