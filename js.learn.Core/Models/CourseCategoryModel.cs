9a377410f581a84915f55a709f606e04bb1a7d34
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-04-01 14:25:49 -0300

    [ :sparkles: ] | create: model - course > category (module::core)

---
+++ js.learn.Core/Models/CourseCategoryModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using js.learn.Core.Entities;
+
+namespace js.learn.Core.Models
+{
+    public class CourseCategoryModel
+    {
+        
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public virtual ICollection<Course> Courses { get; set; } = new List<Course>();
+
+    }
+}
