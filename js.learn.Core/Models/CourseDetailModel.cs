a7edafaa67121ef48f4a351098f5e9cf5523f85f
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-04-01 17:16:20 -0300

    [ :sparkles: ] | create: model - course >  detail (module::core)

---
+++ js.learn.Core/Models/CourseDetailModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace js.learn.Core.Models
+{
+    public class CourseDetailModel: CourseModel 
+    {
+        public List<UserReviewModel>? Reviews { get; set; } = new List<UserReviewModel>();
+        public required List<SessionDetailModel> SessionDetails { get; set; }
+    }
+}
