4a875bacfaa5f2e25dded948824d43b2e2787108
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-04-01 17:18:15 -0300

    [ :sparkle: ] | impl: service - course (module::course)

---
+++ js.learn.Service/CourseService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EllipticCurve;
+using js.learn.Core.Models;
+using js.learn.Data;
+
+namespace js.learn.Service
+{
+    public class CourseService(ICourseRepository repository) : ICourseService
+    {
+        private readonly ICourseRepository _courseRepository = repository;
+
+
+        public Task<List<CourseModel>> GetAll(int? categoryId = null) => _courseRepository.GetAll(categoryId);
+        public Task<CourseModel?> GetDetail(int courseId) => _courseRepository.GetDetail(courseId);
+    }
+}
