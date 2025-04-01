a44b4775e976059ab1e5f2d7760b06df84b2b66b
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-04-01 17:17:01 -0300

    [ :sparkles: ] | create: model - user > rating (module::core)

---
+++ js.learn.Core/Models/UserRatingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace js.learn.Core.Models
+{
+    public class UserRatingModel
+    {
+        public int CourseId { get; set; }
+        public decimal AverageRating { get; set; }
+        public int TotalRating { get; set; }
+    }
+}
