55f97b9b73dfe30381414ba521e75ba09dae3705
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-04-01 12:55:58 -0300

    [ :card_file_box: ] | impl: repository - course > category [async] (module::application)

---
+++ js.learn.Data/AsyncCourseCategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using js.learn.Core.Entities;
+using js.learn.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace js.learn.Data
+{
+    internal class AsyncCourseCategoryRepository(LearnDbContext dbContext) : IAsyncCourseCategoryRepository
+    {
+
+        private readonly ICourseCategoryRepository _courseCategoryRepository;
+
+        public Task<CourseCategory?> GetByIdAsync(int id) => dbContext.CourseCategories.FindAsync(id).AsTask();
+        
+
+        public Task<List<CourseCategory>> GetCourseCategoriesAsync() => dbContext.CourseCategories.ToListAsync();
+    }
+}
