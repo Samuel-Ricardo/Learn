17f37b5feb27b3a627daaf78f7de9561a4fe2e56
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-04-01 17:16:41 -0300

    [ :sparkles: ] | create: model - instructor (module::core)

---
+++ js.learn.Core/Models/InstructorModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace js.learn.Core.Models
+{
+    public class InstructorModel
+    {
+        public int InstructorId { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string? Bio { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
